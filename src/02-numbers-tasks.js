41c3173e6401e642cfe69baf26eb79cf3f74dab4
Breslavets Maria <alwayslearn@mail.ru>
2025-08-29 22:35:35 +0300

    feat: implement new five numbers-functions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -113,8 +113,13 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const numerator = x1 * x2 + y1 * y2;
+  const len1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const len2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const denominator = len1 * len2;
+  const cosTheta = numerator / denominator;
+  return Math.acos(cosTheta);
 }
 
 /**
@@ -129,8 +134,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return Math.abs(value) % 10;
 }
 
 /**
@@ -144,8 +149,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return value - 0;
 }
 
 /**
@@ -161,8 +166,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -182,8 +187,11 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const step1 = num / 10 ** pow;
+  const step2 = Math.round(step1);
+  const step3 = step2 * 10 ** pow;
+  return step3;
 }
 
 /**
@@ -203,8 +211,8 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // return n % 2 >= 0 ? true : false;
 }
 
 /**
